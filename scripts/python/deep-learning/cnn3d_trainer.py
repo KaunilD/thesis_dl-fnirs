b9882cd9c405022d5e447de45a406334d56d671b
kaunild <dhruv.kaunil@gmail.com>
2019-06-27 23:59:42 -0600

    added exp011
    2 channel inputs oxy, dxy. more conv3d blocks with higher planes and dropout layers

--- scripts/python/deep-learning/cnn3d_trainer.py
+++ scripts/python/deep-learning/cnn3d_trainer.py
@@ -1,20 +1,25 @@
 import os
 import sys
+import glob
+import json
 import shutil
+
 import math
 import numpy as np
+import pandas as pd
+
 import torch
 import torch.utils.data as torch_data
 import torch.nn as nn
 import torch.nn.init as init
 import torch.nn.functional as F
-import glob
 from torchsummary import summary
-from sklearn.metrics import confusion_matrix
 
-import plotly.io as pio
-import plotly.plotly as py
-import plotly.graph_objs as go
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set(style="darkgrid")
+
 class MultilabelWMLoader(torch_data.Dataset):
     def __init__(self, data_dir, split, num_classes = 1, time_steps = 160 ):
         self.data_dir = data_dir
@@ -33,9 +38,10 @@ class MultilabelWMLoader(torch_data.Dataset):
             data = np.load(each_file)
             # .reshape((160, 5, 22, 1))
             self.image_list.append(
-                data[0].reshape((1, self.time_steps, 5, 44))
+                data[0].reshape((2, self.time_steps, 5, data[0].shape[3]))
             )
-            self.label_list.append(data[1])
+
+            self.label_list.append(data[1][0])
 
     def __getitem__(self, index):
 
@@ -45,17 +51,21 @@ class MultilabelWMLoader(torch_data.Dataset):
         return len(self.image_list)
 
 
-
 class CNN3D(nn.Module):
-    def __init__(self, num_classes=9, in_planes=1):
+    def __init__(self, num_classes=9, in_planes=2):
         super(CNN3D, self).__init__()
         # N, C, D, H, W = 1, 1, 160, 5, 22
         self.conv1 = nn.Conv3d(in_channels=in_planes, out_channels=5, kernel_size=(65, 1, 2))
         self.bn1 = nn.BatchNorm3d(5)
-        self.conv2 = nn.Conv3d(in_channels=5, out_channels=1, kernel_size=(1, 1, 1))
+        self.conv2 = nn.Conv3d(in_channels=5, out_channels=10, kernel_size=(1, 1, 1))
+        self.bn2 = nn.BatchNorm3d(10)
+        self.conv3 = nn.Conv3d(in_channels=10, out_channels=5, kernel_size=(1, 1, 1))
+        self.bn3 = nn.BatchNorm3d(5)
+        self.conv4 = nn.Conv3d(in_channels=5, out_channels=1, kernel_size=(1, 1, 1))
+        self.bn4 = nn.BatchNorm3d(1)
         self.dropout1 = nn.Dropout(p=0.6)
         self.dropout2 = nn.Dropout(p=0.4)
-        self.fc1   = nn.Linear(460 , 3)
+        self.fc1   = nn.Linear(11 , 3)
         self.softmax = nn.Sigmoid()
 
 
@@ -64,9 +74,30 @@ class CNN3D(nn.Module):
         out = F.max_pool3d(out, 2)
         out = self.bn1(out)
 
+        if self.training:
+            out = self.dropout1(out)
+
 
         out = self.conv2(out)
         out = F.max_pool3d(out, 2)
+        out = self.bn2(out)
+
+        if self.training:
+            out = self.dropout1(out)
+
+        out = self.conv3(out)
+        out = F.max_pool3d(out, 2)
+        out = self.bn3(out)
+
+        if self.training:
+            out = self.dropout1(out)
+
+        out = self.conv4(out)
+        out = F.max_pool3d(out, 2)
+        out = self.bn4(out)
+
+        if self.training:
+            out = self.dropout1(out)
 
 
         out = out.view(out.size(0), -1)
@@ -77,10 +108,11 @@ class CNN3D(nn.Module):
 
         out = self.fc1(out)
 
+        """
         if self.training:
             out = self.dropout2(out)
-
-        out = self.softmax(out)
+        """
+        #out = F.log_softmax(out)
 
         return out
 
@@ -94,11 +126,12 @@ def train(model, dataset_loader, epoch, device, optimizer, criterion):
 
         inputs, labels = data, target
         inputs = inputs.float()
-        labels = labels.float()
+        labels = labels.long()
         inputs, labels = inputs.to(device), labels.to(device)
 
         optimizer.zero_grad()
         outputs = model(inputs)
+
         loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
@@ -106,7 +139,6 @@ def train(model, dataset_loader, epoch, device, optimizer, criterion):
         running_loss += loss.item()
 
     return running_loss/len(dataset_loader.dataset)
-
 
 
 def test(model, dataset_loader, device, criterion):
@@ -120,10 +152,11 @@ def test(model, dataset_loader, device, criterion):
             images, labels = data, target
 
             images = images.float()
-            labels = labels.float()
+            labels = labels.long()
             images, labels = images.to(device), labels.to(device)
 
             outputs = model(images)
+
             valid_loss += criterion(outputs, labels).item()
             # TODO: validation accuracy per label.
             # correct += 1 if pred.eq(labels.byte()).sum().item() == 9 else 0
@@ -140,21 +173,51 @@ def hamming_score(true, pred):
     assert(true.shape == pred.shape)
     return sum((true!=pred))/true.shape[0]
 
-# In[13]:
 
+def get_dataset_distribution(dataset_loader):
+
+    label_bins = {
+        "wm":{0:0, 1:0, 2:0},
+        "vl":{0:0, 1:0, 2:0},
+        "al":{0:0, 1:0, 2:0}
+    }
+
+    for data, target in dataset_loader:
+        target = target.numpy()
+        for t in target:
+
+            label_bins["wm"][t[0]]+=1
+            label_bins["vl"][t[1]]+=1
+            label_bins["al"][t[2]]+=1
+
+    return json.dumps(label_bins, indent=1, sort_keys=True)
+
+
+def plot_losses(loss_history):
+    train_loss = [i[0] for i in loss_history]
+    val_loss = [i[1] for i in loss_history]
+
+    df = pd.DataFrame.from_dict({
+        "train-loss": train_loss,
+        "valid-loss": val_loss
+    })
+
+    plt.figure(figsize=(19, 9))
+    ax = sns.lineplot(data=df)
+    plt.savefig("newplot.png")
 
 if __name__ == '__main__':
+
     torch.cuda.current_device()
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     learning_rate = 1e-3
 
-    criterion = nn.MSELoss(reduction='sum')
+    criterion = nn.CrossEntropyLoss()
+    criterion.to(device)
 
     epochs = 60
     curr_epoch = 0
-
-    criterion.to(device)
 
     loss_history = []
     acc_history = []
@@ -165,9 +228,11 @@ if __name__ == '__main__':
     MODEL_PATH_PREFIX = 'model-cnn3d-epoch'
     MODEL_PATH_EXT = 'pth'
 
+    time_steps = 250
+
     train_dataset = MultilabelWMLoader(
         data_dir='C:\\Users\\dhruv\\Development\\git\\thesis_dl-fnirs\\data\\multilabel',
-        split='train', time_steps = 250
+        split='train', time_steps = time_steps
         )
 
     data_shape = train_dataset[0][0].shape
@@ -177,15 +242,23 @@ if __name__ == '__main__':
         batch_size=8, shuffle=True, num_workers=1
         )
 
+    print("************************TRAIN DISTRIBUTION**********************")
+    #print(get_dataset_distribution(train_loader))
+    print("****************************************************************\n")
+
     val_dataset = MultilabelWMLoader(
         data_dir='C:\\Users\\dhruv\\Development\\git\\thesis_dl-fnirs\\data\\multilabel',
-        split='val', time_steps = 250
+        split='val', time_steps = time_steps
         )
 
     val_loader = torch_data.DataLoader(
         val_dataset,
         batch_size=8, shuffle=True, num_workers=1
         )
+
+    print("************************TEST DISTRIBUTION***********************")
+    #print(get_dataset_distribution(val_loader))
+    print("****************************************************************\n")
 
     model = CNN3D(in_planes = data_shape[0])
 
@@ -199,7 +272,7 @@ if __name__ == '__main__':
     best_score = 0
     best_epoch = 0
 
-    print("************************ MODEL SUMMARY *************************\n")
+    print("************************ MODEL SUMMARY *************************")
     print(summary(model, data_shape))
     print("****************************************************************\n")
 
@@ -213,17 +286,17 @@ if __name__ == '__main__':
             model, val_loader,
             device, criterion
         )
+
         # record all the models that we have had so far.
         loss_history.append((
             running_loss, valid_loss
         ))
-        # write model to disk.
 
+        # write model to disk.
         state = {
             'model': model.state_dict(),
             'optimizer': optimizer.state_dict()
         }
-
         torch.save(
             state,
             MODEL_PATH_PREFIX + '-{}.'.format(curr_epoch) + MODEL_PATH_EXT
@@ -236,36 +309,4 @@ if __name__ == '__main__':
         ))
         curr_epoch+=1
 
-    random_x = np.linspace(1, epochs, epochs)
-    # Create traces
-    trace0 = go.Scatter(
-        x = random_x,
-        y = [i[0] for i in loss_history],
-        mode = 'lines+markers',
-        name = 'train-loss'
-    )
-
-    trace1 = go.Scatter(
-        x = random_x,
-        y = [i[1] for i in loss_history],
-        mode = 'lines+markers',
-        name = 'validation-loss'
-    )
-    data = [trace0, trace1]
-    layout = go.Layout(
-        title= 'TRAINING SNAPSHOT',
-        hovermode= 'closest',
-        xaxis= dict(
-            title= 'EPOCHS',
-            ticklen= 5,
-            zeroline= False,
-            gridwidth= 2,
-        ),
-        yaxis=dict(
-            title= 'LOSS',
-            ticklen= 5,
-            gridwidth= 2,
-        ),)
-    fig = go.Figure(data, layout=layout)
-    py.iplot(fig)
-    pio.write_image(fig, 'newplot.png')
+    plot_losses(loss_history)
