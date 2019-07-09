d7aa2ba7cf2d3dd4f0e7fd109fbd097239ff4bfb
kaunild <dhruv.kaunil@gmail.com>
2019-07-08 19:52:35 -0600

    cleaned up notebooks and commited first lstm experiment

---
+++ scripts/python/deep-learning/experiments/convlstm/001/README.md
@@ -0,0 +1,65 @@
+##### Experiment: 001
+
+
+| hyp-params    | value                    |
+| :------------ | ------------------------ |
+| epochs        | 30                      |
+| loss          | cross entropy loss |
+| optimizer     | Adam                   |
+| learning rate | 0.001                    |
+| accuracy      | negative log liklihood |
+| momentum | 0.7 |
+
+##### Data
+
+| param              | value                                                        |
+| :----------------- | ------------------------------------------------------------ |
+| timesteps          | 55 (5:60)                                                          |
+| normalization      | none                                                         |
+| source experiments | only session 1 from [mindfulness/benchmark_tasks/fNIRS_Data](https://github.com/lmhirshf/mindfulness/tree/master/benchmark_tasks/data/fNIRS_Data) and [Experiments/Experiment7000/](https://github.com/lmhirshf/Experiments/Experiment7000/) |
+| label type         | regression; default3                                         |
+| label config       | [ wm ] => [off = 0, low = 1, high = 2]                       |
+| Shape              | 2x5x11                                                         |
+
+##### Training
+
+![plot](newplot.png)
+
+```
+Epoch   Train Loss      Validation Loss Validation Acc
+0       0.00743         0.00708         68.696
+1       0.00591         0.00626         68.792
+2       0.00546         0.00624         68.986
+3       0.00531         0.00625         68.986
+4       0.00524         0.00636         69.275
+5       0.00519         0.00641         69.179
+6       0.00518         0.00650         68.502
+7       0.00516         0.00657         66.377
+8       0.00513         0.00663         63.671
+9       0.00509         0.00670         62.512
+10      0.00510         0.00678         62.512
+11      0.00508         0.00681         62.319
+12      0.00505         0.00688         62.126
+13      0.00505         0.00693         62.126
+14      0.00501         0.00696         62.029
+15      0.00501         0.00697         61.932
+16      0.00499         0.00708         62.126
+17      0.00498         0.00715         61.932
+18      0.00499         0.00714         61.643
+19      0.00498         0.00713         61.643
+20      0.00496         0.00717         61.449
+21      0.00496         0.00722         61.449
+22      0.00494         0.00726         61.643
+23      0.00491         0.00731         61.546
+24      0.00493         0.00730         61.353
+25      0.00489         0.00728         61.353
+26      0.00490         0.00734         60.966
+27      0.00488         0.00738         61.643
+28      0.00487         0.00741         61.256
+29      0.00488         0.00744         61.256
+30      0.00486         0.00743         61.063
+```
+
+##### Observations
+
+1. Simple model, it works
---
+++ scripts/python/deep-learning/lstm_trainer.py
@@ -0,0 +1,387 @@
+
+# coding: utf-8
+
+# In[4]:
+
+
+import os
+import sys
+import glob
+import json
+import shutil
+
+import math
+import numpy as np
+import pandas as pd
+
+import torch
+import torch.utils.data as torch_data
+import torch.nn as nn
+import torch.nn.init as init
+import torch.nn.functional as F
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set(style="darkgrid")
+
+
+# In[46]:
+
+
+class LSTMDataLoader(torch_data.Dataset):
+    def __init__(self, data_list ):
+        self.data_list = data_list
+        self.image_list, self.label_list = [], []
+        self.read_lists()
+
+    def read_lists(self):
+        for item in self.data_list:
+            datum = np.load(item).item()
+
+            image = datum["data"]
+            #image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2], image.shape[3] ))
+            label = datum["class"]
+
+            self.image_list.append(image[5:60])
+            self.label_list.append(datum["wl_label"][0])
+
+    def __getitem__(self, index):
+        return tuple([self.image_list[index], self.label_list[index]])
+
+
+    def __len__(self):
+        return len(self.image_list)
+
+
+# In[47]:
+
+
+data_list = glob.glob('C:\\Users\\dhruv\\Development\\git\\thesis_dl-fnirs\\data\\multilabel\\all\\*.npy')
+train_dataloader = LSTMDataLoader(data_list[:int(0.8*len(data_list))])
+val_dataloader = LSTMDataLoader(data_list[int(0.8*len(data_list)):])
+
+
+# In[48]:
+
+
+class ConvLSTM2D(nn.Module):
+    def __init__(
+            self,
+            input_size, input_channel, hidden_channel,
+            kernel_size, stride=1, padding=0
+    ):
+        """
+        Initializations
+        :param input_size: (int, int): height, width tuple of the input
+        :param input_channel: int: number of channels of the input
+        :param hidden_channel: int: number of channels of the hidden state
+        :param kernel_size: int: size of the filter
+        :param stride: int: stride
+        :param padding: int: width of the 0 padding
+        """
+
+        super(ConvLSTM2D, self).__init__()
+        self.n_h, self.n_w = input_size
+        self.n_c = input_channel
+        self.hidden_channel = hidden_channel
+
+        self.conv_xi = nn.Conv2d(
+            self.n_c,
+            self.hidden_channel,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+        )
+
+        self.conv_xf = nn.Conv2d(
+            self.n_c,
+            self.hidden_channel,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+        )
+
+        self.conv_xo = nn.Conv2d(
+            self.n_c,
+            self.hidden_channel,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+        )
+
+        self.conv_xg = nn.Conv2d(
+            self.n_c,
+            self.hidden_channel,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+        )
+
+        self.conv_hi = nn.Conv2d(
+            self.hidden_channel,
+            self.hidden_channel,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+        )
+
+        self.conv_hf = nn.Conv2d(
+            self.hidden_channel,
+            self.hidden_channel,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+        )
+
+        self.conv_ho = nn.Conv2d(
+            self.hidden_channel,
+            self.hidden_channel,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+        )
+
+        self.conv_hg = nn.Conv2d(
+            self.hidden_channel,
+            self.hidden_channel,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+        )
+
+        self.conv_hi = nn.Conv2d(
+            self.hidden_channel,
+            self.hidden_channel,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+        )
+
+    def forward(self, x, hidden_states):
+        """
+        Forward prop.
+        reference: https://arxiv.org/pdf/1506.04214.pdf (3.1)
+        :param x: input tensor of shape (n_batch, n_c, n_h, n_w)
+        :param hidden_states: (tensor, tensor) for hidden and cell states.
+                              Each of shape (n_batch, n_hc, n_hh, n_hw)
+        :return: (hidden_state, cell_state)
+        """
+
+        hidden_state, cell_state = hidden_states
+
+        xi = self.conv_xi(x)
+        hi = self.conv_hi(hidden_state)
+        xf = self.conv_xf(x)
+        hf = self.conv_hf(hidden_state)
+        xo = self.conv_xo(x)
+        ho = self.conv_ho(hidden_state)
+        xg = self.conv_xg(x)
+        hg = self.conv_hg(hidden_state)
+
+        i = torch.sigmoid(xi + hi)
+        f = torch.sigmoid(xf + hf)
+        o = torch.sigmoid(xo + ho)
+        g = torch.tanh(xg + hg)
+
+        cell_state = f * cell_state + i * g
+        hidden_state = o * torch.tanh(cell_state)
+
+        return hidden_state, cell_state
+
+    def init_hidden(self, batch_size):
+        return (
+                torch.zeros(batch_size, self.hidden_channel, self.n_h, self.n_w).cuda(),
+                torch.zeros(batch_size, self.hidden_channel, self.n_h, self.n_w).cuda()
+               )
+
+
+# In[58]:
+
+
+class ConvLSTMNet(nn.Module):
+    def __init__( self, num_classes = 2):
+        super(ConvLSTMNet, self).__init__()
+        self.convLSTM2d1 = ConvLSTM2D((5, 11), 2, 2, 1)
+        self.fc1 = nn.Linear(110, 3)
+
+    def forward(self, x, hidden_states = None):
+
+        b_idx, ts, n_ch, w, h = x.size()
+        if hidden_states:
+            self.h1, self.c1 = hidden_states
+        else:
+            self.h1, self.c1 = self.convLSTM2d1.init_hidden(batch_size=b_idx)
+
+        for t in range(ts):
+            self.h1, self.c1 = self.convLSTM2d1(
+                x[:, t, :, :, :], (self.h1, self.c1)
+            )
+
+        out = self.fc1(self.h1.view(self.h1.size(0), -1))
+
+        """
+
+            h1, c1 = self.convLSTM2d1(
+                x[0], (self.h1, self.c1)
+            )
+            self.h1, self.c1 = h1[-1], c1[-1]
+            out = self.fc1(self.h1.view(self.h1.size(0), -1))
+
+        """
+
+        return out
+
+
+
+# In[59]:
+
+
+def train(model, dataset_loader, epoch, device, optimizer, criterion):
+    model.train()
+    running_loss = 0.0
+    total = 0
+    for i , (data, target) in enumerate(dataset_loader):
+        inputs, labels = data.float(), target.long()
+        inputs, labels = inputs.to(device), labels.to(device)
+        optimizer.zero_grad()
+
+        outputs = model(inputs)
+
+        loss = criterion(outputs, labels)
+
+        loss.backward()
+
+        optimizer.step()
+
+        running_loss += loss.item()
+        total += labels.size(0)
+
+        """
+        print('Train: [Epoch: {}/{}, Batch: {} ({:.0f}%)]'
+              .format(
+                  epoch,
+                  NUM_EPOCHS,
+                  i + 1,
+                  i*100/len(train_loader)
+              ), end='\r')
+        """
+    return running_loss/total
+
+
+def test(model, dataset_loader, device, criterion):
+    model.eval()
+    correct = 0
+    total = 0
+    valid_loss = 0
+
+    with torch.no_grad():
+        for i, (data, target) in enumerate(dataset_loader):
+
+            images, labels = data.float(), target.long()
+            images, labels = images.to(device), labels.to(device)
+
+            outputs = model(images)
+
+            valid_loss += criterion(outputs, labels).item()
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    accuracy = 100 * correct / total
+    valid_loss /= total
+    return valid_loss, accuracy
+
+
+# In[62]:
+
+
+def plot_losses(loss_history):
+    train_loss = [i[0] for i in loss_history]
+    val_loss = [i[1] for i in loss_history]
+
+    df = pd.DataFrame.from_dict({
+        "train-loss": train_loss,
+        "valid-loss": val_loss
+    })
+
+    plt.figure(figsize=(19, 9))
+    ax = sns.lineplot(data=df)
+    plt.savefig("newplot.png")
+
+
+if __name__ == '__main__':
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = ConvLSTMNet()
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3, amsgrad=False)
+
+    epochs = 30
+    curr_epoch = 0
+
+    model.to(device)
+    criterion.to(device)
+
+    loss_history = []
+    acc_history = []
+
+    best_accuracy = 0.0
+    best_model = -1
+
+    MODEL_PATH_PREFIX = 'model-convlstm-epoch'
+    MODEL_PATH_EXT = 'pth'
+
+    train_loader = torch_data.DataLoader(
+        train_dataloader,
+        batch_size=128, shuffle=True, num_workers=1
+    )
+
+    val_loader = torch_data.DataLoader(
+        val_dataloader,
+        batch_size=128, shuffle=False, num_workers=1
+    )
+
+    is_best = True
+    best_score = 0
+    best_epoch = 0
+
+    print("Epoch\tTrain Loss\tValidation Loss\tValidation Acc")
+    while curr_epoch <= epochs:
+        running_loss = train(
+            model, train_loader,
+            curr_epoch, device, optimizer, criterion
+        )
+        valid_loss, accuracy = test(
+            model, val_loader,
+            device, criterion
+        )
+        # record all the models that we have had so far.
+        loss_history.append((
+            running_loss, valid_loss
+        ))
+
+        acc_history.append(accuracy)
+        # write model to disk.
+
+        state = {
+            'model': model.state_dict(),
+            'optimizer': optimizer.state_dict()
+        }
+
+        'models/model-lenet-epoch'
+        torch.save(
+            state,
+            MODEL_PATH_PREFIX + '-{}.'.format(curr_epoch) + MODEL_PATH_EXT
+        )
+
+        print('{}\t{:.5f}\t\t{:.5f}\t\t{:.3f}\t\t'.format(
+            curr_epoch,
+            running_loss,
+            valid_loss,
+            accuracy
+        ))
+        curr_epoch+=1
+
+    plot_losses(loss_history)
